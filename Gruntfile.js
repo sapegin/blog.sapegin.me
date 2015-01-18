04427543852d756e9c17cadd17cc632e735b54a6
Artem Sapegin <artem@sapegin.ru>
2015-01-18 15:10:39 +0300

    Gruntfile: coffee → js.

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+// gruntjs.com
+
+module.exports = function(grunt) {
+	'use strict'
+
+	var debug = !!grunt.option('debug');
+
+	require('tamia-grunt')(grunt, {
+		tamia: {
+			author: 'Artem Sapegin, http://sapegin.me',
+			src: 'src',
+			dest: 'src/files',
+			imagesSrc: 'src/files/images_src',
+			imagesDest: 'src/files/images',
+			modernizr: false,
+			stylobuild: {
+				autoprefixer: {
+					browsers: 'last 2 versions, ie 9'
+				}
+			}
+		},
+		concat: {
+			main: {
+				nonull: true,
+				src: [
+					'<%= bower_concat.main.dest %>'
+				],
+				dest: '<%= tamia.dest %>/build/scripts.js',
+			}
+		}
+	});
+
+	grunt.registerTask('default', ['scripts', 'styles', 'images']);
+	grunt.registerTask('deploy', ['scripts', 'styles']);
+
+};
