1a1bb127ab47433c031d756b5d8852c084dcae43
Artem Sapegin <artem@sapegin.ru>
2015-10-29 08:08:04 +0100

    Use classList.

--- js/inlines/search-form.js
+++ js/inlines/search-form.js
@@ -1,19 +1,10 @@
 (function() {
-	function toggle(elem, cls) {
-		var classes = elem.className;
-		if ((' ' + classes + ' ').indexOf(cls) === -1) {
-			elem.className += ' ' + cls;
-		}
-		else {
-			elem.className = classes.replace(new RegExp('(^|\\s)' + cls + '(\\s|$)'), ' ').replace(/\s$/, '');
-		}
-	}
 	var link = document.getElementById('search-link');
 	link.addEventListener('click', function(event) {
 		event.preventDefault();
 		var form = document.getElementById('search');
-		toggle(form, 'is-hidden');
-		toggle(link, 'is-active');
+		form.classList.toggle('is-hidden');
+		link.classList.toggle('is-active');
 		link.blur();
 		form.querySelector('.js-field').focus();
 	});
