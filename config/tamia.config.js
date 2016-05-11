de2776d35e8bfb36f96ef9c14f165f6887d8d402
Artem Sapegin <artem@sapegin.ru>
2016-05-11 20:36:59 +0200

    Update Tamia and tamia-build.

--- config/tamia.config.js
+++ config/tamia.config.js
@@ -3,7 +3,8 @@ module.exports = function(options) {
 	if (argv.lang) {
 		// Redirect HTML pages to local folder
 		options.rewrites = [
-			{ from: '^([^\.]*)$', to: '/' + argv.lang + '$1' },
+			'^/$ /' + argv.lang + '/index.html L',
+			'^([^.]*)$ /' + argv.lang + '$1.html',
 		];
 	}
 
