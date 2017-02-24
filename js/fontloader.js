3b5fd5acdd4c285f418ae05836254d9e8b293e16
Artem Sapegin <artem@sapegin.ru>
2017-02-24 22:13:16 +0100

    Remove WOFF fonts

--- js/fontloader.js
+++ js/fontloader.js
@@ -1,78 +1,53 @@
 // Based on http://bdadam.com/blog/better-webfont-loading-with-localstorage-and-woff2.html
 // This script must be placed in the HEAD above all external stylesheet declarations (link[rel=stylesheet])
 window.loadFont = function(fontName, fontUrlBase) {
-	// 0. Many unsupported browsers should stop here
-	const ua = navigator.userAgent;
-	const noSupport =
-		// IE8 and below
-		!window.addEventListener ||
-		// Android Stock Browser below 4.4 and Opera Mini
-		(ua.match(/(Android (2|3|4.0|4.1|4.2|4.3))|(Opera (Mini|Mobi))/) && !ua.match(/Chrome/))
-	;
-	if (noSupport) {
+	// Very optimistic check for fonts support
+	if (!('FontFace' in window)) {
 		return;
 	}
 
-	// 1. Setting up localStorage
-	let loSto = {};
+	// Setting up localStorage
+	let storage = {};
 	try {
-		// We set up a proxy variable to help with localStorage, e.g. when cookies are disabled
-		// and the browser prevents us accessing it.
-		// Otherwise some exceptions can be thrown which completely prevent font loading.
-		loSto = localStorage || {};
+		// We set up a proxy variable to help with localStorage, e.g. when cookies are disabled and the browser prevents
+		// us accessing it. Otherwise some exceptions can be thrown which completely prevent font loading.
+		storage = localStorage || {};
 	}
-	catch (ex) {
+	catch (err) {
 		/* */
 	}
 
 	const localStoragePrefix = 'font-' + fontName;
 	const localStorageUrlKey = localStoragePrefix + 'url';
 	const localStorageCssKey = localStoragePrefix + 'css';
-	const storedFontUrlBase = loSto[localStorageUrlKey];
-	const storedFontCss = loSto[localStorageCssKey];
+	const storedFontCss = storage[localStorageCssKey];
 
-	// 2. Setting up the <style> element, that we are using to apply the base64 encoded font data
+	// Setting up the <style> element, that we are using to apply the base64 encoded font data
 	const styleElement = document.createElement('style');
 	styleElement.rel = 'stylesheet';
 	document.head.appendChild(styleElement);
-	// Setting styleElement.textContent must be after this line, because of IE9 errors
 
-	// 3. Checking whether the font data is already in localStorage and up-to-date
-	if (storedFontCss && storedFontUrlBase === fontUrlBase) {
-		// the css is still in the localStorage AND it was loaded from one of the current URLs
+	// Checking whether the font data is already in localStorage and up-to-date
+	if (storedFontCss && storage[localStorageUrlKey] === fontUrlBase) {
+		// The css is still in the localStorage AND it was loaded from one of the current URLs
 
-		// 4. Applying the font style sheet
+		// Applying the font style sheet
 		styleElement.textContent = storedFontCss;
 	}
 	else {
 		// The data was not present, or loaded from an obsolete URL
 		// So we have to load it again
 
-		// 5. Checking for WOFF2 support to know which URL we should use
-		const url = fontUrlBase + '.woff' + (hasWoff2() ? '2' : '') + '.css';
-
-		// 6. Fetching the font data from the server
+		// Fetching the font data from the server
 		const request = new XMLHttpRequest();
-		request.open('GET', url);
-		request.onload = function() {
+		request.open('GET', `${fontUrlBase}.woff2.css`);
+		request.onload = () => {
 			if (request.status >= 200 && request.status < 400) {
-				// 7. Updating localStorage with the fresh data and applying the font data
-				loSto[localStorageUrlKey] = fontUrlBase;
-				loSto[localStorageCssKey] = styleElement.textContent = request.responseText;
+				// Updating localStorage with the fresh data and applying the font data
+				storage[localStorageUrlKey] = fontUrlBase;
+				storage[localStorageCssKey] = styleElement.textContent = request.responseText;
 			}
 		};
 		request.send();
-	}
-
-	function hasWoff2() {
-		// Source: https://github.com/filamentgroup/woff2-feature-test
-		if (!window.FontFace) {
-			return false;
-		}
-
-		const f = new FontFace('t', 'url("data:application/font-woff2,") format("woff2")', {});
-		f.load();
-
-		return f.status === 'loading';
 	}
 };
