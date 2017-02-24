0a79a33103e552491ef159a1cfea7aa91ec06a5a
Artem Sapegin <artem@sapegin.ru>
2017-02-24 22:19:53 +0100

    Update Yandex Metrika code

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,31 @@
-(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter412115 = new Ya.Metrika({id:412115, webvisor:true, clickmap:true, trackLinks:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");
+(function(d, w, c) {
+	(w[c] = w[c] || []).push(function() {
+			try {
+				w.yaCounter412115 = new Ya.Metrika({
+						id: 412115,
+						clickmap: true,
+						trackLinks: true,
+						accurateTrackBounce: true
+					}
+				);
+			}
+			catch (e) {
+			}
+		}
+	);
+
+	var n = d.getElementsByTagName("script")[0],
+		s = d.createElement("script"),
+		f = function() {
+			n.parentNode.insertBefore(s, n);
+		};
+	s.async = true;
+	s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+	if (w.opera == "[object Opera]") {
+		d.addEventListener("DOMContentLoaded", f, false);
+	}
+	else {
+		f();
+	}
+})(document, window, "yandex_metrika_callbacks");
