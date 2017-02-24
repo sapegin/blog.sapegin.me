f8e9bda696e5a5a6d7c6df006b23c03cd2ba6b47
Artem Sapegin <artem@sapegin.ru>
2017-02-24 21:18:43 +0100

    New header

--- templates/components/Header.jsx
+++ templates/components/Header.jsx
@@ -1,12 +1,10 @@
 import Block from 'tamia/lib/components/Block';
-import Logo from './Logo';
 import Nav from './Nav';
 import SearchForm from './SearchForm';
 
 export default function() {
 	return (
 		<Block component="header" bottom={4}>
-			<Logo />
 			<Nav />
 			<SearchForm />
 		</Block>
--- templates/components/Nav.jsx
+++ templates/components/Nav.jsx
@@ -1,27 +1,22 @@
 import Link from 'tamia/lib/components/Link';
+import Group from 'tamia/lib/components/Group';
 import s from './Nav.pcss';
 
 export default function(props, children, { url, translation, option, __ }) {
 	const translationUrl = option('translationHost') + (translation ? url : '');
 	return (
-		<nav class={s.root}>
-			<div class={s.item}>
-				{url === '/' ? (
-					__('allPosts')
-				) : (
-					<Link href="/">{__('allPosts')}</Link>
-				)}
-			</div>
-			<div class={s.item}>
-				<Link id="search-link" component="button" type="button">
-					{__('search')}
-				</Link>
-			</div>
-			<div class={s.item}>
-				<Link href={translationUrl}>
-					{__('translation')}
-				</Link>
-			</div>
-		</nav>
+		<Group component="nav" separator="·" class={s.root}>
+			{url === '/' ? (
+				<h1 class={s.item}>{__('title')}</h1>
+			) : (
+				<Link class={s.item} href="/">{__('title')}</Link>
+			)}
+			<Link class={s.item} id="search-link" component="button" type="button">
+				{__('search')}
+			</Link>
+			<Link class={s.item} href={translationUrl}>
+				{__('translation')}
+			</Link>
+		</Group>
 	);
 }
