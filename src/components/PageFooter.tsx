fc9f5553684e28d9ce82159f71b28f115629af80
Artem Sapegin <artem@sapegin.ru>
2020-06-04 15:58:02 +0200

    Upgrade Tâmia

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Stack, Box } from 'tamia';
+import { Grid, Box } from 'tamia';
 import About from './About';
 import Subscription from './Subscription';
 
 export default function PostFooter() {
 	return (
-		<Stack gridGap="m" mb="m" minColumnWidth={320}>
+		<Grid gridGap="m" mb="m" minColumnWidth={320}>
 			<Box as="aside" mb="m" aria-label="About the author">
 				<About />
 			</Box>
 			<Box as="aside" mb="m" aria-label="Newsletter">
 				<Subscription />
 			</Box>
-		</Stack>
+		</Grid>
 	);
 }
